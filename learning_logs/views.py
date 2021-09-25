11964f0b42a28c5b4c493e60f9f474e952040c96
Carlos Pinheiro <carlosadap@gmail.com>
2021-09-25 09:08:17 +0200

    Add auth to new entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -23,9 +23,8 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user.
-    if topic.owner != request.user:
-        raise Http404
-        
+    check_topic_owner(request, topic)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -40,6 +39,9 @@ def new_topic(request):
         # POST data submitted; process data.
         form = TopicForm(data=request.POST)
         if form.is_valid():
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             form.save()
             return redirect('learning_logs:topics')
     # Display a blank or invalid form.
@@ -50,7 +52,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
-
+    check_topic_owner(request, topic)
+    
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
@@ -72,6 +75,7 @@ def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
@@ -85,3 +89,7 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
