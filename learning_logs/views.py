72efc2f096eb22992352c667136f4d2031a12017
Carlos Pinheiro <carlosadap@gmail.com>
2021-09-25 13:04:20 +0200

    Fix bugs and edit the rest of htmls

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -53,7 +53,7 @@ def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
     check_topic_owner(request, topic)
-    
+
     if request.method != 'POST':
         # No data submitted; create a blank form.
         form = EntryForm()
